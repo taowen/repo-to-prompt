dd2821bff1e2b6829317db456585c51802cdb742
Tao Wen <taowen@gmail.com>
2024-05-02 10:04:48 +0800

    update

---
+++ repo-to-prompt.codemod.js
@@ -0,0 +1,10 @@
+
+/**
+ * @param {vscode} vscode the entry to vscode plugin api
+ * @param {vscode.Uri} selectedFile currently selected file in vscode explorer
+ * @param {vscode.Uri[]} selectedFiles currently multi-selected files in vscode explorer
+ */
+async function run(vscode, selectedFile, selectedFiles) {
+    console.log('you can debug the script with console.log')
+}
+await run(vscode, selectedFile, selectedFiles);
